9271b728f85d536119fcbd665d126828ff07a312
lzw-gif <lzw1798@g.rit.edu>
2026-06-16 16:57:02 -0400

    Update test_thermorawfileparser.py

--- tests/integrationtests/wrappers/test_thermorawfileparser.py
+++ tests/integrationtests/wrappers/test_thermorawfileparser.py
@@ -3,41 +3,6 @@ import pytest
 
 import urgap
 
-#
-# @pytest.mark.slow
-# @pytest.mark.skipif(
-#     urgap.init_node("thermo_raw_file_parser_2_0_0").has_all_required_installations()
-#     is False,
-#     reason="Node Requires {0}".format(
-#         urgap.init_node("thermo_raw_file_parser_2_0_0").META_INFO["requires"]
-#     ),
-# )
-#
-# pytest.mark.parametrize(
-#     "provide_clean_node_dirs",
-#     [
-#         (
-#             urgap.UFile(
-#                 uri=f"file://{urgap._test_folder}/data?uftype="
-#                 f"{urgap.uftypes.proteomics.THERMO_RAW}#mzML/BSA1.raw"
-#             ),
-#             urgap.URunDict(
-#                 {
-#                     "parameters": {
-#                         "ThermoRawFileParser:2.0.0": {},
-#                     },
-#                     "unode_parameters": {
-#                         "force": True,
-#                     },
-#                 }
-#             ),
-#             ["ThermoRawFileParser:2.0.0"],
-#         )
-#     ],
-#     indirect=["provide_clean_node_dirs"],
-# )
-
-
 def test_wrapper_thermorawfileparser_2_0_0_simple(tmp_dir):
     ufiles = urgap.UFileList(
         [
