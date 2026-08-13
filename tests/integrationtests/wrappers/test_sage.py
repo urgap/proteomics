2f5f35c37b5ed852f843036a0f851be8e6c97f56
Lewis Winterberger (RIT Student) <lzw1798@sporcsubmit.rc.rit.edu>
2026-08-13 18:40:42 -0400

    bugfix/functionality

--- tests/integrationtests/wrappers/test_sage.py
+++ tests/integrationtests/wrappers/test_sage.py
@@ -7,7 +7,8 @@ import pytest
 
 
 def test_searchdb_sage(tmp_path: Path) -> None:
-    params = urgap.UFile(uri=f"file:///{urgap._test_folder}/data/sage_params?uftype={urgap.uftypes.proteomics.params.SAGE_JSON}#sage.params")
+    params = urgap.UFile(uri=f"file:///{urgap._test_folder}/data/sage_params?uftype={urgap.uftypes.proteomics.params.SAGE_JSON}#sage_config.json")
+    mzml = urgap.UFile(uri=f"file:///{urgap._test_folder}/data/mzml?uftype={urgap.uftypes.ms.converter.mzml.THERMORAWPARSER_MZML}#LQSRPAAPPAPGPGQLTLR.mzML")
     urd = urgap.URunDict(
         {
             "parameters": {"Sage:0.14.7": {
@@ -19,12 +20,12 @@ def test_searchdb_sage(tmp_path: Path) -> None:
         },
     )
     search_node = urgap.init_node("Sage:0.14.7")
-    search_result = search_node.run([params], urd)
+    search_result = search_node.run([params, mzml], urd)
     assert search_result[0].path.exists() is True
 
     # check that output is not empty
     df = pd.read_csv(search_result[0].path, delimiter="\t")
-    assert df.shape[0] > 13
+    assert df.shape[0] == 1
 
     search_node.remove_output_folder(output_file=search_result[0])
 
